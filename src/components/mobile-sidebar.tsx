e411a144e3885acec24e78aa9115680f925c4e69
LawrenceDarko <darkolawrence@gmail.com>
2023-10-28 14:50:38 +0000

    refactor code

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -9,27 +9,11 @@ import Logo from "./logo";
 export default function TemplateDemo() {
     const [visible, setVisible] = useState<boolean>(false);
 
-    const customIcons = (
-        <React.Fragment>
-            <button className="p-sidebar-icon p-link mr-2">
-                <span className="pi pi-print" />
-            </button>
-            <button className="p-sidebar-icon p-link mr-2">
-                <span className="pi pi-search" />
-            </button>
-        </React.Fragment>
-    );
-
-    const customHeader = (
-        <div className="pt-5">
-            <Logo />
-        </div>
-    );
 
     return (
-        <div className="card flex justify-content-center">
-            <HiMenuAlt2 onClick={()=> setVisible(true)} className="text-white text-2xl md:hidden"/>
-            <Sidebar visible={visible} onHide={() => setVisible(false)} className="text-white pt-3">
+        <div className="flex card justify-content-center">
+            <HiMenuAlt2 onClick={()=> setVisible(true)} className="text-2xl text-white md:hidden"/>
+            <Sidebar visible={visible} onHide={() => setVisible(false)} className="pt-3 text-white">
                 <SidebarMain />
             </Sidebar>
             <Button icon="pi pi-plus" className="text-white" onClick={() => setVisible(true)} />
